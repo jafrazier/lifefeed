3c0b36595559581af31feb8d64d5ded7594ae5ba
jfrazier <johnfrazier1222@gmail.com>
2018-08-19 14:56:19 -0400

    login,signup, and member function setup

--- server.rb
+++ server.rb
@@ -10,5 +10,48 @@ get '/' do
   erb :home
 end
 
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  p params
+  user = User.new(
+    email: params['email'],
+    name: params['name'],
+    password: params['password']
+  )
+  user.save
+  redirect '/'
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  email = params['email']
+  given_password = params['password']
+  user = User.find_by(email: email)
+  if user.password == given_password
+    session[:user] = user
+    redirect :member
+  else
+    p 'invalid'
+    redirect '/'
+  end
+end
+
+get '/member' do
+  erb :member
+end
+
+get '/logout' do
+  session[:user] = nil
+  p 'user has been logged out'
+  redirect '/'
+end
+
+
 
 require './models'
