96e9b2e210117f6d03e053b4a9abee4672b951a3
jfrazier <johnfrazier1222@gmail.com>
2018-08-19 13:03:41 -0400

    Database created

---
+++ db/migrate/20180819165633_create_users_table.rb
@@ -0,0 +1,12 @@
+class CreateUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+    t.string :email
+    t.string :name
+    t.string :password_hash
+    t.datetime :created_at
+    t.datetime :upadated_at
+  end
+  add_index :users, :email, unique: true
+  end
+end
--- server.rb
+++ server.rb
@@ -4,7 +4,11 @@ require 'httparty'
 require "json"
 enable :sessions
 
+set :database, "sqlite3:lifefeed.sqlite3"
 
 get '/' do
   erb :home
 end
+
+
+require './models'
