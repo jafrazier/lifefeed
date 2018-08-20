4dd4786bb122a7a9a3e0700417ea8d5bffd28041
jfrazier <johnfrazier1222@gmail.com>
2018-08-20 11:25:53 -0400

    newsfeed added

--- test.rb
+++ test.rb
@@ -1,11 +1,14 @@
 require 'httparty'
 require 'json'
+require 'date'
 
-request = HTTParty.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=10629f6795fb4c9b8f408cf4e3f81374')
+request = HTTParty.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=10629f6795fb4c9b8f408cf4e3f81374')
 
 news = JSON.parse(request.body)
 
 article = news['articles']
-article.each do |news|
-puts news['title']
-end
+p article.first['source']['name']
+
+# article.each do |news|
+# news['title']
+# end
